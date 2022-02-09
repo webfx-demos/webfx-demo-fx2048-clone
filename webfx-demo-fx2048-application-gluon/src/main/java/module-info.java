c62de33d28dc6754a54e0382f4d93dd2b88d28c9
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-09 13:59:11 +0000

    Changed module token from "javafx" to "openjfx" when designating OpenJFX runtime

--- webfx-demo-fx2048-application-gluon/src/main/java/module-info.java
+++ webfx-demo-fx2048-application-gluon/src/main/java/module-info.java
@@ -4,9 +4,10 @@ module webfx.demo.fx2048.application.gluon {
 
     // Direct dependencies modules
     requires webfx.demo.fx2048.application;
-    requires webfx.kit.javafx;
+    requires webfx.kit.openjfx;
     requires webfx.platform.java.appcontainer.impl;
     requires webfx.platform.java.resource.impl;
+    requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.java.storage.impl;
     requires webfx.platform.shared.log.impl.simple;
--- webfx-demo-fx2048-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-fx2048-application-openjfx/src/main/java/module-info.java
@@ -1,12 +1,13 @@
 // File managed by WebFX (DO NOT EDIT MANUALLY)
 
-module webfx.demo.fx2048.application.javafx {
+module webfx.demo.fx2048.application.openjfx {
 
     // Direct dependencies modules
     requires webfx.demo.fx2048.application;
-    requires webfx.kit.javafx;
+    requires webfx.kit.openjfx;
     requires webfx.platform.java.appcontainer.impl;
     requires webfx.platform.java.resource.impl;
+    requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.java.storage.impl;
     requires webfx.platform.shared.log.impl.simple;
