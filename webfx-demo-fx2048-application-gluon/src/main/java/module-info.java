c66d2c49857b0040a31aee6949c54cfe724ad4b0
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-24 20:17:34 +0000

    Renamed appcontainer to boot in module names and java code

--- webfx-demo-fx2048-application-gluon/src/main/java/module-info.java
+++ webfx-demo-fx2048-application-gluon/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module webfx.demo.fx2048.application.gluon {
     // Direct dependencies modules
     requires webfx.demo.fx2048.application;
     requires webfx.kit.openjfx;
-    requires webfx.platform.java.appcontainer.impl;
+    requires webfx.platform.java.boot.impl;
     requires webfx.platform.java.resource.impl;
     requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
--- webfx-demo-fx2048-application-gwt/src/main/resources/super/java/util/ServiceLoader.java
+++ webfx-demo-fx2048-application-gwt/src/main/resources/super/java/util/ServiceLoader.java
@@ -15,9 +15,9 @@ public class ServiceLoader<S> implements Iterable<S> {
             case "dev.webfx.platform.client.services.storage.spi.SessionStorageProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.storage.spi.impl.GwtSessionStorageProvider::new);
             case "dev.webfx.platform.client.services.uischeduler.spi.UiSchedulerProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.uischeduler.spi.impl.GwtUiSchedulerProvider::new);
             case "dev.webfx.platform.gwt.services.resource.spi.impl.GwtResourceBundle": return new ServiceLoader<S>();
-            case "dev.webfx.platform.shared.services.appcontainer.spi.ApplicationContainerProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.appcontainer.spi.impl.GwtApplicationContainerProvider::new);
-            case "dev.webfx.platform.shared.services.appcontainer.spi.ApplicationJob": return new ServiceLoader<S>();
-            case "dev.webfx.platform.shared.services.appcontainer.spi.ApplicationModuleInitializer": return new ServiceLoader<S>(dev.webfx.kit.launcher.WebFxKitLauncherModuleInitializer::new, dev.webfx.platform.shared.services.appcontainer.spi.impl.ApplicationJobsStarter::new, dev.webfx.platform.gwt.services.resource.spi.impl.GwtResourceModuleInitializer::new);
+            case "dev.webfx.platform.shared.services.boot.spi.ApplicationBooterProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.boot.spi.impl.GwtApplicationBooterProvider::new);
+            case "dev.webfx.platform.shared.services.boot.spi.ApplicationJob": return new ServiceLoader<S>();
+            case "dev.webfx.platform.shared.services.boot.spi.ApplicationModuleBooter": return new ServiceLoader<S>(dev.webfx.kit.launcher.WebFxKitLauncherModuleBooter::new, dev.webfx.platform.shared.services.boot.spi.impl.ApplicationJobsBooter::new, dev.webfx.platform.gwt.services.resource.spi.impl.GwtResourceModuleBooter::new);
             case "dev.webfx.platform.shared.services.log.spi.LoggerProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.log.spi.impl.GwtLoggerProvider::new);
             case "dev.webfx.platform.shared.services.resource.spi.ResourceServiceProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.resource.spi.impl.GwtResourceServiceProvider::new);
             case "dev.webfx.platform.shared.services.scheduler.spi.SchedulerProvider": return new ServiceLoader<S>(dev.webfx.platform.gwt.services.uischeduler.spi.impl.GwtUiSchedulerProvider::new);
